e1a944c4108b8008b7007044b74223d2fcb76a88
xodnd0507 <24457545yong@gmail.com>
2024-08-07 13:52:26 +0900

    style : Desgin login form

---
+++ login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>로그인</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="join_area">
+        <div class="login_modal">
+            <h1>로그인</h1>
+            <form action="loginProcess.php" method="post">
+                <p><input type="text" name="userid"  placeholder="아이디"> </p>
+                <p><input type="password" name="userpw" placeholder="비밀번호"></p>
+                <p class="login_btn_area">
+                    <button type="submit" class="join_submit">로그인</button>
+                    <button type="reset">취소</button>
+                </p>
+            </form> 
+        </div>
+    </div>
+    <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+    <script src="js/index.js"></script>
+</body>
+</html>
